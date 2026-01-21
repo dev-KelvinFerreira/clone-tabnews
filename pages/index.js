942d1c7d6ea8168a0516ce17ec003ae711abaa5b
Kelvin Ferreira <ferreirakelvin33@gmail.com>
2026-01-21 02:19:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Eu te amo minha princesa. Se você me ama, da uma risadinha me manda uma fotinha!🤭</h1>
+function Home() {
+  return (
+    <h1>
+      Eu te amo minha princesa. Se você me ama, da uma risadinha me manda uma
+      fotinha!🤭
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
